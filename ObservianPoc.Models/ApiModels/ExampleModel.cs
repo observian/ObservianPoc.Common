08e069e73524699ded7686f95ef23a43d33561b0
Adan Pineda <adan.pineda@live.com>
2020-09-16 17:30:07 -0600

    First Commit

---
+++ ObservianPoc.Models/ApiModels/ExampleModel.cs
@@ -0,0 +1,11 @@
+namespace ObservianPoc.Models.ApiModels
+{
+    public class ExampleModel
+    {
+        public string Property1 { get; set; }
+        
+        public string Property2 { get; set; }
+
+        public string Property3 { get; set; }
+    }
+}
